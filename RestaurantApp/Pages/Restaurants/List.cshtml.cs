353c176bb77dd29e9d2e219330a15748c1308a78
DavidBermejoS <dbermejosimon@gmail.com>
2020-12-11 13:54:38 +0100

    F-001: Injecting models and using configuration properties

--- RestaurantApp/Pages/Restaurants/List.cshtml.cs
+++ RestaurantApp/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace RestaurantApp.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
+        public string Message { get; set; }
+
+        public string Message_properties { get; set; }
+
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         public void OnGet()
         {
+            Message = "Hello World!";
+            Message_properties = config["Message"];
         }
     }
 }
