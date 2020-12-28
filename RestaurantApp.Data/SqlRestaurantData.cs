411525d17009880dd283747054727ddda7ae1581
DavidBermejoS <dbermejosimon@gmail.com>
2020-12-28 12:25:45 +0100

    F-004: Created and implemented database access layer in a C# class

---
+++ RestaurantApp.Data/SqlRestaurantData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using RestaurantApp.Core;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantApp.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly RestaurantAppDbContext db;
+
+        public SqlRestaurantData(RestaurantAppDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            db.Add(restaurant);
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+           return db.SaveChanges();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = GetById(id);
+            if(restaurant != null)
+            {
+                db.Restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return db.Restaurants.Find(id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantByName(string name)
+        {
+            var query = from r in db.Restaurants
+                        where r.Name.StartsWith(name) || string.IsNullOrEmpty(name)
+                        orderby r.Name
+                        select r;
+            return query;
+        }
+
+        public Restaurant Update(Restaurant restaurant)
+        {
+            var entity = db.Restaurants.Attach(restaurant);
+            entity.State = EntityState.Modified;
+            return restaurant;
+        }
+    }
+}
