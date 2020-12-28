569d132de92062efd025d7073d7e767b92735947
David Bermejo Simón <dbermejosimon@gmail.com>
2020-12-28 12:15:00 +0100

    F-004: Refactor Data implementation into another class file and add delete method

--- RestaurantApp.Data/IRestaurantData.cs
+++ RestaurantApp.Data/IRestaurantData.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Linq;
 using RestaurantApp.Core;
 
 namespace RestaurantApp.Data
@@ -12,59 +10,7 @@ namespace RestaurantApp.Data
         Restaurant GetById(int id);
         Restaurant Update(Restaurant restaurant);
         Restaurant Add(Restaurant restaurant);
+        Restaurant Delete(int id);
         int Commit();
-    }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        List<Restaurant> restaurants;
-
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-            {
-                new Restaurant{Id = 1, Name = "Scott's Pizza", Location = "Maryland",Cuisine = CuisineType.Italian },
-                new Restaurant{Id = 2, Name = "Cinnamon Club", Location = "London",Cuisine = CuisineType.Indian},
-                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican }
-
-            };
-        }
-
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-
-        public IEnumerable<Restaurant> GetRestaurantByName(string name)
-        {
-            return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                   orderby r.Name
-                   select r;
-        }
-
-        public Restaurant Update(Restaurant updatedRestaurant)
-        {
-            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
-            if(restaurant != null)
-            {
-                restaurant.Name = updatedRestaurant.Name;
-                restaurant.Location = updatedRestaurant.Location;
-                restaurant.Cuisine = updatedRestaurant.Cuisine;
-            }
-            return restaurant;
-        }
-        public int Commit()
-        {
-            //this method will have a real meaning when a real datasource will be implemented.
-            return 0;
-        }
-
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
-            return newRestaurant;
-        }
     }
 }
