561a9a46199004d4b63acaeb6db4021215f19806
DavidBermejoS <dbermejosimon@gmail.com>
2020-12-31 14:14:33 +0100

    F-006: Adapt the code to a real deployment state with migration of db

--- RestaurantApp/Program.cs
+++ RestaurantApp/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestaurantApp.Data;
 
 namespace RestaurantApp
 {
@@ -13,10 +16,21 @@ namespace RestaurantApp
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			var host = CreateHostBuilder(args).Build();
+            MigrateDatabase(host);
+            host.Run();
 		}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static void MigrateDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<RestaurantAppDbContext>();
+                db.Database.Migrate();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
