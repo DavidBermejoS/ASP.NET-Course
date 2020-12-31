abaf83b42ea112f6a9de471ac3ac5b2eb076f10f
DavidBermejoS <dbermejosimon@gmail.com>
2020-12-31 13:29:52 +0100

    F-006: Implemented logger service to the list model.

--- RestaurantApp/Pages/Restaurants/List.cshtml.cs
+++ RestaurantApp/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using RestaurantApp.Core;
 using RestaurantApp.Data;
 
@@ -14,6 +15,7 @@ namespace RestaurantApp.Pages.Restaurants
     {
         private readonly IConfiguration config;
         private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
 
         public string Message { get; set; }
         public string Message_properties { get; set; }
@@ -22,17 +24,27 @@ namespace RestaurantApp.Pages.Restaurants
         [BindProperty(SupportsGet =true)]
         public string SearchTerm { get; set; }
 
-        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        public ListModel(IConfiguration config,
+                            IRestaurantData restaurantData,
+                            ILogger<ListModel> logger)
         {
             this.config = config;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
 
         public void OnGet()
         {
+            logger.LogInformation("[INFO LOGGER] --> Executing List Model");
             //Message = "Hello World!";
             //Message_properties = config["Message"]; 
             Restaurants = restaurantData.GetRestaurantByName(SearchTerm);
+            logger.LogInformation("[INFO LOGGER] --> Restaurants obtained in db:");
+            foreach (Restaurant r in Restaurants.ToList())
+            {
+                logger.LogInformation("[INFO LOGGER]--> Restaurant nº" + r.Id + "{ Name: " + r.Name + "; Location: " + r.Location + "; Cuisine: " + r.Cuisine + "}");
+            }
+
         }
     }
 }
